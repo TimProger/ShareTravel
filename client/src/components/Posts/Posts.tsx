1fed71e59f257467da4ff102cb0839e3af78ef2f
TimProger <akylverentimofei@mail.ru>
2022-04-17 19:59:18 +0300

    Добавил комментарии для компоненты posts

--- client/src/components/Posts/Posts.tsx
+++ client/src/components/Posts/Posts.tsx
@@ -1,23 +1,34 @@
 import React, {useEffect} from "react"
+
+// Импортирую стили
 import './Posts.css'
+// Импортирую компоненту поста
 import Post from "./Post/Post";
+// Импортирую функцию для создания контейнерной компоненты
 import {connect} from "react-redux";
+// Импортирую ActionCreator'ы
 import {fetchPosts, likePost, loadComments} from "../../store/action-creators/post";
 
 function Posts(props: any){
 
+    // Получаю пользователей путём вызова ActionCreator
     useEffect(() => {
         props.fetchPosts()
     }, [])
 
+    // Проверяю статус загрузки
     if (props.loading) {
+        // Если статус true, то пользователь будет видеть этот код
         return <h1>Идет загрузка...</h1>
     }
 
+    // Проверяю статус ошибки
     if (props.error) {
+        // Если есть ошибка, то пользователь будет видеть этот код
         return <h1>{props.error}</h1>
     }
 
+    // Вывожу посты, создавая под каждого компоненту Post
     return (
         <div className="posts">
             {props.posts.map((el: any, index: number)=>{
@@ -27,6 +38,7 @@ function Posts(props: any){
     )
 }
 
+// Получаю пропсы, которые должен передать в компоненту
 let mapStateToProps = (state: any) => {
     return {
         posts: state.post.posts,
@@ -35,6 +47,8 @@ let mapStateToProps = (state: any) => {
     }
 }
 
+// Создаю контейнерную компоненту PostsContainer при помощи функции connect и
+// передаю в неё необходимые компоненте ActionCreator'ы и пропсы
 let PostsContainer = connect(mapStateToProps, {
     fetchPosts,
     likePost,
