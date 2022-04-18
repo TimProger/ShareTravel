f7334f4b3fb48554d5787da7567c0f09dc80ec9d
TimProger <akylverentimofei@mail.ru>
2022-04-18 22:54:26 +0300

    Окончательно избавился от контейнерных компонент

--- client/src/components/Posts/Posts.tsx
+++ client/src/components/Posts/Posts.tsx
@@ -4,55 +4,39 @@ import React, {useEffect} from "react"
 import './Posts.css'
 // Импортирую компоненту поста
 import Post from "./Post/Post";
-// Импортирую функцию для создания контейнерной компоненты
-import {connect} from "react-redux";
-// Импортирую ActionCreator'ы
-import {fetchPosts, likePost, loadComments} from "../../store/actionCreators/postAC";
+// Импортирую изменённый для работы с типами хук useSelector
+import {useTypedSelector} from "../../hooks/useTypedSelector";
+import {useActions} from "../../hooks/useActions";
 
-function Posts(props: any){
+function Posts(){
+    const {posts, error, loading} = useTypedSelector(state => state.post)
+    const {fetchPosts, likePost, loadComments} = useActions()
 
     // Получаю пользователей путём вызова ActionCreator
     useEffect(() => {
-        props.fetchPosts()
+        fetchPosts()
     }, [])
 
     // Проверяю статус загрузки
-    if (props.loading) {
+    if (loading) {
         // Если статус true, то пользователь будет видеть этот код
         return <h1>Идет загрузка...</h1>
     }
 
     // Проверяю статус ошибки
-    if (props.error) {
+    if (error) {
         // Если есть ошибка, то пользователь будет видеть этот код
-        return <h1>{props.error}</h1>
+        return <h1>{error}</h1>
     }
 
     // Вывожу посты, создавая под каждого компоненту Post
     return (
         <div className="posts">
-            {props.posts.map((el: any, index: number)=>{
-                return <Post loadComments={props.loadComments} likePost={props.likePost} key={index} post={el}/>
+            {posts.map((el: any, index: number)=>{
+                return <Post loadComments={loadComments} likePost={likePost} key={index} post={el}/>
             })}
         </div>
     )
 }
 
-// Получаю пропсы, которые должен передать в компоненту
-let mapStateToProps = (state: any) => {
-    return {
-        posts: state.post.posts,
-        error: state.post.error,
-        loading: state.post.loading
-    }
-}
-
-// Создаю контейнерную компоненту PostsContainer при помощи функции connect и
-// передаю в неё необходимые компоненте ActionCreator'ы и пропсы
-let PostsContainer = connect(mapStateToProps, {
-    fetchPosts,
-    likePost,
-    loadComments,
-})(Posts);
-
-export default PostsContainer;
+export default Posts;
