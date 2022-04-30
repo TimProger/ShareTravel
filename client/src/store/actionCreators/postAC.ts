fa0e4d3101d0839687519dfb420d9bfbb472a351
TimProger <akylverentimofei@mail.ru>
2022-04-30 18:05:41 +0300

    Вместо 100 постов теперь подгружаются только 10

--- client/src/store/actionCreators/postAC.ts
+++ client/src/store/actionCreators/postAC.ts
@@ -10,10 +10,13 @@ export const fetchPosts = () => {
             dispatch({type: PostActionTypes.FETCH_POSTS})
             fetch('https://jsonplaceholder.typicode.com/posts/')
                 .then(response => response.json())
-                .then(json => dispatch({
-                    type: PostActionTypes.FETCH_POSTS_SUCCESS,
-                    payload: json
-                })).catch(e => dispatch({
+                .then(json => {
+                    json = json.slice(0, 10);
+                    dispatch({
+                        type: PostActionTypes.FETCH_POSTS_SUCCESS,
+                        payload: json
+                    })
+                }).catch(e => dispatch({
                         type: PostActionTypes.FETCH_POSTS_ERROR,
                         payload: 'Произошла ошибка при загрузке постов'
                     })
