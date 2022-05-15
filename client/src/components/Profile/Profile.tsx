301470a66af21b78ba48fb9013e112fede5bb83a
TimProger <akylverentimofei@mail.ru>
2022-05-15 15:57:48 +0300

    Добавил компоненту NotFound

--- client/src/components/Profile/Profile.tsx
+++ client/src/components/Profile/Profile.tsx
@@ -3,8 +3,8 @@ import React from "react"
 import './Profile.css';
 import { FiMapPin } from "react-icons/fi";
 import {isEmpty} from "../../utils/utils";
-import {Navigate} from "react-router-dom";
 import {IProfileProps} from "../../types/profileType";
+import NotFound from "../NotFound/NotFound";
 
 function Profile(props: IProfileProps) {
 
@@ -32,7 +32,7 @@ function Profile(props: IProfileProps) {
 
     // Проверяю объект на наличие свойств
     if(isEmpty(props.profile)){
-        return <Navigate to={'/'} />
+        return <NotFound />
     }
 
     return (
@@ -60,69 +60,6 @@ function Profile(props: IProfileProps) {
             </div>
         </div>
     )
-}
-
-
-class Profile1 extends React.Component<IProfileProps, {}> {
-
-    // Получаю пользователя используя полученный id
-    componentDidMount() {
-        this.props.fetchProfile(this.props.id)
-    }
-
-    // Удаляю пользователя
-    componentWillUnmount() {
-        this.props.dropProfile()
-    }
-
-    render() {
-
-        // Проверяю статус загрузки
-        if (this.props.loading) {
-            return (
-                <div className="page">
-                    <h1>Идет загрузка...</h1>
-                </div>
-            )
-        }
-
-        // Проверяю статус ошибки
-        if (this.props.error) {
-            return <h1>{this.props.error}</h1>
-        }
-
-        // Проверяю объект на наличие свойств
-        if(isEmpty(this.props.profile)){
-            return <Navigate to={'/'} />
-        }
-
-
-        return (
-            <div className="profile">
-                <div className="profile-background">
-                    <img className="profile-background-img" src="https://images.unsplash.com/photo-1585170466274-9e8dc02e14d3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NXx8YXdlc29tZSUyMHBpY3xlbnwwfHwwfHw%3D&w=1000&q=80" alt="background"/>
-                    <div className="">
-                        <img className="profile-avatar-img" src="https://upload.wikimedia.org/wikipedia/commons/5/5f/Alberto_conversi_profile_pic.jpg" alt="avatar"/>
-                    </div>
-                    <div className="profile-name-surname">{this.props.profile.name}</div>
-                    <div className="profile-text">{this.props.profile.company.catchPhrase}</div>
-                    <div className="profile-city-text"><FiMapPin/>{this.props.profile.address.street} {this.props.profile.address.suite}</div>
-                    <div className="sub_follow">
-                        <div className="sub_follow__btn sub"><span>followers</span>1000</div>
-                        <div className="sub_follow__btn follow"><span>following</span>1000 </div>
-                    </div>
-                    <div className="btn-container">
-                        <div className="btn-follow__container">
-                            <button className="btn-follow">Подписаться</button>
-                        </div>
-                        <div className="btn-message__container">
-                            <button className="btn-message">Сообщение</button>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        )
-    }
 }
 
 export default Profile;
--- client/src/components/pages/Main/MainPage.tsx
+++ client/src/components/pages/Main/MainPage.tsx
@@ -7,6 +7,7 @@ import Followers from "../../Followers/FollowersContainer";
 import Profile from "../../Profile/ProfileContainer";
 import {useActions} from "../../../hooks/useActions";
 import Exit from "../../Exit/Exit";
+import NotFound from "../../NotFound/NotFound";
 
 function MainPage(){
     const {logout} = useActions()
@@ -20,12 +21,11 @@ function MainPage(){
                 <Routes>
                     <Route path='/auth' element={<Navigate to="/" />} />
                     <Route path='/registration' element={<Navigate to="/" />} />
-                    <Route path='*' element={<Navigate to="/" />} />
+                    <Route path='*' element={<NotFound />} />
                     <Route path='/' element={<Posts />} />
                     <Route path='/followers' element={<Followers />} />
                     <Route path='/user/:id' element={<Profile />} />
                     <Route path='/exit' element={<Exit />} />
-
                     {/*<Route path='/dialogs/*' element={<Dialogs />} />*/}
                 </Routes>
             </div>
