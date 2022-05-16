d7f39ecb0de104dd4ea97c9dbbbeefa58a3e4566
TimProger <akylverentimofei@mail.ru>
2022-05-17 02:50:20 +0300

    Подправил стили компоненты follower

--- client/src/store/actionCreators/authAC.ts
+++ client/src/store/actionCreators/authAC.ts
@@ -4,29 +4,29 @@ import AuthService from "../../components/Auth/AuthService";
 
 const users = [
     {
-        id: 0,
-        avatar: 'https://i.ytimg.com/vi/LmWQd8zhEg4/maxresdefault.jpg',
-        name: 'Tim',
-        surname: 'Timmsky',
-        text: 'What a good network',
+        id: 1,
+        avatar: 'https://upload.wikimedia.org/wikipedia/commons/5/5f/Alberto_conversi_profile_pic.jpg',
+        name: 'Leanne',
+        surname: 'Graham',
+        text: 'Multi-layered client-server neural-net',
         email: '1email1@mail.ru',
         age: 23
     },
     {
-        id: 1,
+        id: 2,
         avatar: 'https://i.ytimg.com/vi/LmWQd8zhEg4/maxresdefault.jpg',
-        name: 'Bob',
-        surname: 'Luminsky',
-        text: 'Yo dudes',
+        name: 'Ervin',
+        surname: 'Howell',
+        text: 'Proactive didactic contingency',
         email: '2email2@mail.ru',
         age: 51
     },
     {
-        id: 2,
+        id: 3,
         avatar: 'https://i.ytimg.com/vi/LmWQd8zhEg4/maxresdefault.jpg',
-        name: 'Steven',
-        surname: 'Armstrong',
-        text: 'Nice knife, Jack.',
+        name: 'Clementine',
+        surname: 'Bauch',
+        text: 'Face to face bifurcated interface',
         email: '3email3@mail.ru',
         age: 42
     }
