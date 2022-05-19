cca8478385c07ec19bea559f103d9c53d8b1e65a
TimProger <akylverentimofei@mail.ru>
2022-05-20 00:44:55 +0300

    Убрал старые моковые данные

--- client/src/store/actionCreators/profileAC.ts
+++ client/src/store/actionCreators/profileAC.ts
@@ -19,43 +19,6 @@ export const fetchProfile = (id: string, page:number=1) => {
                     payload: 'Произошла ошибка при поиске профиля: ' + e
                 })
             )
-            // setTimeout(()=> {
-            //     const users = [
-            //         {
-            //             id: 0,
-            //             avatar: 'https://i.ytimg.com/vi/LmWQd8zhEg4/maxresdefault.jpg',
-            //             name: 'Tim',
-            //             surname: 'Timmsky',
-            //             text: 'What a good network',
-            //             email: '1email1@mail.ru',
-            //             age: 23
-            //         },
-            //         {
-            //             id: 1,
-            //             avatar: 'https://i.ytimg.com/vi/LmWQd8zhEg4/maxresdefault.jpg',
-            //             name: 'Bob',
-            //             surname: 'Luminsky',
-            //             text: 'Yo dudes',
-            //             email: '2email2@mail.ru',
-            //             age: 51
-            //         },
-            //         {
-            //             id: 2,
-            //             avatar: 'https://i.ytimg.com/vi/LmWQd8zhEg4/maxresdefault.jpg',
-            //             name: 'Steven',
-            //             surname: 'Armstrong',
-            //             text: 'Nice knife, Jack.',
-            //             email: '3email3@mail.ru',
-            //             age: 42
-            //         }
-            //     ]
-            //     const user = users.find(user=>user.id === +id)
-            //     dispatch({
-            //         type: ProfileActionTypes.FETCH_PROFILE_SUCCESS,
-            //         payload: user
-            //     })
-            // }, 10)
-
         } catch (e) {
             dispatch({
                 type: ProfileActionTypes.FETCH_PROFILE_ERROR,
--- client/src/store/actionCreators/userAC.ts
+++ client/src/store/actionCreators/userAC.ts
@@ -26,22 +26,6 @@ export const fetchUsers = (page: number = 1) => {
                     })
                 }
             )
-            // fetch(`https://randomuser.me/api/?page=${page}&results=40&seed=abc`)
-            //     .then(response => response.json())
-            //     .then(json => {
-            //         const json1 = json.results.slice(0, json.results.length / 2)
-            //         const json2 = json.results.slice(json.results.length / 2, json.results.length)
-            //         dispatch({
-            //             type: UserActionTypes.FETCH_USERS_SUCCESS,
-            //             payload: [json1, json2]
-            //         });
-            //     }).catch(e => {
-            //         dispatch({
-            //             type: UserActionTypes.FETCH_USERS_ERROR,
-            //             payload: 'Произошла ошибка при запросе к серверу' + e
-            //         })
-            //     }
-            // )
         } catch (e) {
             dispatch({
                 type: UserActionTypes.FETCH_USERS_ERROR,
